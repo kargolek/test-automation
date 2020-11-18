39b666164641355ca52e37da951f30fe52a01d95
karol_orlowicz <karol.orlowicz@gmail.com>
2020-11-18 11:12:23 +0100

    add remote driver

--- qagui/src/test/java/com/jsystem/testautomation/qagui/ConfigLocalSelenium.java
+++ qagui/src/test/java/com/jsystem/testautomation/qagui/ConfigLocalSelenium.java
@@ -1,13 +1,18 @@
 package com.jsystem.testautomation.qagui;
 
 import com.jsystem.testautomation.qagui.classic.FrontendTest;
+import com.jsystem.testautomation.qagui.config.GuiConfig;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
@@ -21,15 +26,19 @@ public class ConfigLocalSelenium {
     }
 
     @BeforeEach
-    public void setup() throws URISyntaxException {
-        driver = new ChromeDriver();
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        desiredCapabilities.setPlatform(Platform.LINUX);
+        desiredCapabilities.setVersion("");
+
+        driver = new RemoteWebDriver(new URL("http://172.20.224.1:4444/wd/hub/"), desiredCapabilities);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
     }
 
     @AfterEach
-    public void  tearDown(){
+    public void tearDown(){
         driver.quit();
     }
 
